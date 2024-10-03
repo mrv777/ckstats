3b142713998ad2055e5e9714b0744625eb97a59c
mrv777 <mrv777@users.noreply.github.com>
2024-10-03 17:15:25 -0500

    fix: Update themes

--- components/ThemeController.tsx
+++ components/ThemeController.tsx
@@ -2,7 +2,40 @@
 
 import { useEffect, useState } from 'react';
 
-const themes = ['light', 'dark', 'cupcake'];
+const themes = [
+  'dark',
+  'light',
+  'cupcake',
+  'bumblebee',
+  'emerald',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'valentine',
+  'halloween',
+  'garden',
+  'forest',
+  'aqua',
+  'lofi',
+  'pastel',
+  'fantasy',
+  'wireframe',
+  'black',
+  'luxury',
+  'dracula',
+  'cmyk',
+  'autumn',
+  'business',
+  'acid',
+  'lemonade',
+  'night',
+  'coffee',
+  'winter',
+  'dim',
+  'nord',
+  'sunset',
+];
 
 export default function ThemeController() {
   const [theme, setTheme] = useState('light');
@@ -21,17 +54,27 @@ export default function ThemeController() {
   };
 
   return (
+    
     <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn m-1">
-        Theme
-      </label>
-      <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-        {themes.map((t) => (
-          <li key={t}>
-            <a onClick={() => handleThemeChange({ target: { value: t } } as any)}>{t}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+          <label tabIndex={0} className="btn m-1">
+            Theme
+            <svg width="12px" height="12px" className="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow-2xl bg-base-200 rounded-box w-52 max-h-96 overflow-y-auto">
+            {themes.map((t) => (
+              <li key={t}>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                  aria-label={t}
+                  value={t}
+                  checked={theme === t}
+                  onClick={() => handleThemeChange({ target: { value: t } } as any)}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
   );
 }
