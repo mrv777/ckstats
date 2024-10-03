155fa717d0b8266f95fca0fcc9f21621eb12423c
mrv777 <mrv777@users.noreply.github.com>
2024-10-03 16:37:01 -0500

    fix: update migration

--- prisma/migrations/20241003213643_init/migration.sql
+++ prisma/migrations/20241003213643_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "PoolStats" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" SERIAL NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "runtime" INTEGER NOT NULL,
     "users" INTEGER NOT NULL,
     "workers" INTEGER NOT NULL,
@@ -14,27 +14,31 @@ CREATE TABLE "PoolStats" (
     "hashrate6hr" TEXT NOT NULL,
     "hashrate1d" TEXT NOT NULL,
     "hashrate7d" TEXT NOT NULL,
-    "diff" REAL NOT NULL,
+    "diff" DOUBLE PRECISION NOT NULL,
     "accepted" BIGINT NOT NULL,
     "rejected" BIGINT NOT NULL,
     "bestshare" BIGINT NOT NULL,
-    "SPS1m" REAL NOT NULL,
-    "SPS5m" REAL NOT NULL,
-    "SPS15m" REAL NOT NULL,
-    "SPS1h" REAL NOT NULL
+    "SPS1m" DOUBLE PRECISION NOT NULL,
+    "SPS5m" DOUBLE PRECISION NOT NULL,
+    "SPS15m" DOUBLE PRECISION NOT NULL,
+    "SPS1h" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "PoolStats_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "User" (
-    "address" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "authorised" BIGINT NOT NULL DEFAULT 0
+    "address" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorised" BIGINT NOT NULL DEFAULT 0,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("address")
 );
 
 -- CreateTable
 CREATE TABLE "UserStats" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "userAddress" TEXT NOT NULL,
     "hashrate1m" TEXT NOT NULL DEFAULT '0',
     "hashrate5m" TEXT NOT NULL DEFAULT '0',
@@ -44,32 +48,36 @@ CREATE TABLE "UserStats" (
     "lastShare" BIGINT NOT NULL DEFAULT 0,
     "workerCount" INTEGER NOT NULL DEFAULT 0,
     "shares" BIGINT NOT NULL DEFAULT 0,
-    "bestShare" REAL NOT NULL DEFAULT 0,
+    "bestShare" DOUBLE PRECISION NOT NULL DEFAULT 0,
     "bestEver" BIGINT NOT NULL DEFAULT 0,
-    "timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserStats_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Worker" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "hashrate1m" TEXT NOT NULL DEFAULT '0',
     "hashrate5m" TEXT NOT NULL DEFAULT '0',
     "hashrate1hr" TEXT NOT NULL DEFAULT '0',
     "hashrate1d" TEXT NOT NULL DEFAULT '0',
     "hashrate7d" TEXT NOT NULL DEFAULT '0',
-    "lastUpdate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastUpdate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "shares" BIGINT NOT NULL DEFAULT 0,
-    "bestShare" REAL NOT NULL DEFAULT 0,
+    "bestShare" DOUBLE PRECISION NOT NULL DEFAULT 0,
     "bestEver" BIGINT NOT NULL DEFAULT 0,
-    "userAddress" TEXT NOT NULL
+    "userAddress" TEXT NOT NULL,
+
+    CONSTRAINT "Worker_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "WorkerStats" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "workerId" INTEGER NOT NULL,
     "hashrate1m" TEXT NOT NULL DEFAULT '0',
     "hashrate5m" TEXT NOT NULL DEFAULT '0',
@@ -77,9 +85,11 @@ CREATE TABLE "WorkerStats" (
     "hashrate1d" TEXT NOT NULL DEFAULT '0',
     "hashrate7d" TEXT NOT NULL DEFAULT '0',
     "shares" BIGINT NOT NULL DEFAULT 0,
-    "bestShare" REAL NOT NULL DEFAULT 0,
+    "bestShare" DOUBLE PRECISION NOT NULL DEFAULT 0,
     "bestEver" BIGINT NOT NULL DEFAULT 0,
-    "timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WorkerStats_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -93,3 +103,12 @@ CREATE UNIQUE INDEX "Worker_userAddress_name_key" ON "Worker"("userAddress", "na
 
 -- CreateIndex
 CREATE INDEX "WorkerStats_timestamp_idx" ON "WorkerStats"("timestamp");
+
+-- AddForeignKey
+ALTER TABLE "UserStats" ADD CONSTRAINT "UserStats_userAddress_fkey" FOREIGN KEY ("userAddress") REFERENCES "User"("address") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Worker" ADD CONSTRAINT "Worker_userAddress_fkey" FOREIGN KEY ("userAddress") REFERENCES "User"("address") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkerStats" ADD CONSTRAINT "WorkerStats_workerId_fkey" FOREIGN KEY ("workerId") REFERENCES "Worker"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
