d6a95fcf6c3848de907d1269be97f36785129738
mrv777 <mrv777@users.noreply.github.com>
2024-10-05 09:43:04 -0500

    fix: Add space before units

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,19 +2,19 @@ export function formatNumber(num: number | bigint): string {
   const absNum = Math.abs(Number(num));
   
   if (absNum >= 1e21) {
-    return (Number(num) / 1e21).toFixed(2) + 'S';
+    return (Number(num) / 1e21).toFixed(2) + ' S';
   } else if (absNum >= 1e18) {
-    return (Number(num) / 1e18).toFixed(2) + 'Qi';
+    return (Number(num) / 1e18).toFixed(2) + ' Qi';
   } else if (absNum >= 1e15) {
-    return (Number(num) / 1e15).toFixed(2) + 'Q';
+    return (Number(num) / 1e15).toFixed(2) + ' Q';
   } else if (absNum >= 1e12) {
-    return (Number(num) / 1e12).toFixed(2) + 'T';
+    return (Number(num) / 1e12).toFixed(2) + ' T';
   } else if (absNum >= 1e9) {
-    return (Number(num) / 1e9).toFixed(2) + 'B';
+    return (Number(num) / 1e9).toFixed(2) + ' B';
   } else if (absNum >= 1e6) {
-    return (Number(num) / 1e6).toFixed(2) + 'M';
+    return (Number(num) / 1e6).toFixed(2) + ' M';
   } else if (absNum >= 1e3) {
-    return (Number(num) / 1e3).toFixed(2) + 'K';
+    return (Number(num) / 1e3).toFixed(2) + ' K';
   } else {
     return num.toLocaleString();
   }
@@ -23,15 +23,15 @@ export function formatNumber(num: number | bigint): string {
 export function formatHashrate(num: string | bigint | number): string {
   const absNum = Math.abs(Number(num));
   if (absNum >= 1e15) {
-    return (Number(num) / 1e15).toFixed(0) + 'PH/s';
+    return (Number(num) / 1e15).toFixed(0) + ' PH/s';
   } else if (absNum >= 1e12) {
-    return (Number(num) / 1e12).toFixed(0) + 'TH/s';
+    return (Number(num) / 1e12).toFixed(0) + ' TH/s';
   } else if (absNum >= 1e9) {
-    return (Number(num) / 1e9).toFixed(0) + 'GH/s';
+    return (Number(num) / 1e9).toFixed(0) + ' GH/s';
   } else if (absNum >= 1e6) {
-    return (Number(num) / 1e6).toFixed(0) + 'MH/s';
+    return (Number(num) / 1e6).toFixed(0) + ' MH/s';
   } else if (absNum >= 1e3) {
-    return (Number(num) / 1e3).toFixed(0) + 'KH/s';
+    return (Number(num) / 1e3).toFixed(0) + ' KH/s';
   } else {
     return num.toLocaleString();
   }
