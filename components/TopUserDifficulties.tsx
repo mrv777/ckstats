d8b480773967f79e3ce482ba68017a9dbc7f1485
mrv777 <mrv777@users.noreply.github.com>
2024-10-06 17:43:15 -0500

    fix: Label inactive with top diff

--- components/TopUserDifficulties.tsx
+++ components/TopUserDifficulties.tsx
@@ -53,14 +53,42 @@ export default async function TopUserDifficulties({
               <tbody>
                 {difficulties.map((user, index) => (
                   <tr key={user.address}>
-                    <td>{index + 1}</td>
+                    <td>
+                      {index + 1}{' '}
+                      {user.workerCount === 0 ? (
+                        <span
+                          className="text-warning tooltip tooltip-right"
+                          data-tip="No active workers"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4 inline-block stroke-current"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                            />
+                          </svg>
+                        </span>
+                      ) : (
+                        ''
+                      )}
+                    </td>
                     <td>
                       {user.address.slice(0, 6)}...{user.address.slice(-4)}
                     </td>
 
                     {limit > SMALL_LIMIT ? (
                       <>
-                        <td>{user.workerCount}</td>
+                        <td
+                          className={`${user.workerCount === 0 ? 'text-error' : ''}`}
+                        >
+                          {user.workerCount}
+                        </td>
                         <td className="text-accent">
                           {formatNumber(Number(user.difficulty))}
                         </td>
